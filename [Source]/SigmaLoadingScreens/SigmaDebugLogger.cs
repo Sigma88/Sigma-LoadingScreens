0eadd71b63223e345ac849072c022b1dd15f3ff0
Sigma88 <sigmamodexpansions@outlook.com>
2017-10-17 02:44:22 +0200

    add debug logger

---
+++ [Source]/SigmaLoadingScreens/SigmaDebugLogger.cs
@@ -0,0 +1,24 @@
+﻿namespace Sigma88LoadingScreensPlugin
+{
+    internal static class Debug
+    {
+        internal static bool debug = true;
+        static string Tag = "[SigmaLog LS]";
+
+        internal static void Log(string message)
+        {
+            if (debug)
+            {
+                UnityEngine.Debug.Log(Tag + ": " + message);
+            }
+        }
+
+        internal static void Log(string Method, string message)
+        {
+            if (debug)
+            {
+                UnityEngine.Debug.Log(Tag + " " + Method + ": " + message);
+            }
+        }
+    }
+}
