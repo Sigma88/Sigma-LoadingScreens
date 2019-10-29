0fcdbdaddac44ab84dc363c735abdaa5098a9679
Sigma88 <sigmamodexpansions@outlook.com>
2019-10-30 00:21:22 +0100

    allow builtin

--- [Source]/SigmaLoadingScreens/LoadingScreens.cs
+++ [Source]/SigmaLoadingScreens/LoadingScreens.cs
@@ -8,32 +8,32 @@ namespace Sigma88LoadingScreensPlugin
 {
     internal static class LoadingScreens
     {
-        //internal static void LoadBuiltIn(string[] mods)
-        //{
-        //    if (LoadingScreenSettings.newScreens == null)
-        //        LoadingScreenSettings.newScreens = new List<Object>();
-        //    if (LoadingScreenSettings.newTips == null)
-        //        LoadingScreenSettings.newTips = new List<string>();
+        internal static void LoadBuiltIn(string[] mods)
+        {
+            if (LoadingScreenSettings.newScreens == null)
+                LoadingScreenSettings.newScreens = new List<Object>();
+            if (LoadingScreenSettings.newTips == null)
+                LoadingScreenSettings.newTips = new List<string>();
 
-        //    if (mods.Contains("GalacticNeighborhood"))
-        //    {
-        //        Debug.Log("LoadBuiltIn", "GN detected");
-        //        LoadingScreenSettings.newScreens.AddRange(LoadScreens("GalacticNeighborhood/LoadingScreens/PluginData/"));
-        //        LoadingScreenSettings.newTips.Add("Populating Star Systems...");
-        //    }
-        //    if (mods.Contains("SigmaBinary"))
-        //    {
-        //        Debug.Log("LoadBuiltIn", "SB detected");
-        //        LoadingScreenSettings.newScreens.AddRange(LoadScreens("Sigma/Binary/LoadingScreens/PluginData/"));
-        //        LoadingScreenSettings.newTips.Add("Re-centering Barycenters...");
-        //    }
-        //    if (mods.Contains("SigmaDimensions"))
-        //    {
-        //        Debug.Log("LoadBuiltIn", "SD detected");
-        //        LoadingScreenSettings.newScreens.AddRange(LoadScreens("Sigma/Dimensions/LoadingScreens/PluginData/"));
-        //        LoadingScreenSettings.newTips.Add("Scrambling Universal Constants...");
-        //    }
-        //}
+            if (mods.Contains("GalacticNeighborhood"))
+            {
+                Debug.Log("LoadBuiltIn", "GN detected");
+                LoadingScreenSettings.newScreens.AddRange(LoadScreens("GalacticNeighborhood/LoadingScreens/PluginData/"));
+                LoadingScreenSettings.newTips.Add("Populating Star Systems...");
+            }
+            if (mods.Contains("SigmaBinary"))
+            {
+                Debug.Log("LoadBuiltIn", "SB detected");
+                LoadingScreenSettings.newScreens.AddRange(LoadScreens("Sigma/Binary/LoadingScreens/PluginData/"));
+                LoadingScreenSettings.newTips.Add("Re-centering Barycenters...");
+            }
+            if (mods.Contains("SigmaDimensions"))
+            {
+                Debug.Log("LoadBuiltIn", "SD detected");
+                LoadingScreenSettings.newScreens.AddRange(LoadScreens("Sigma/Dimensions/LoadingScreens/PluginData/"));
+                LoadingScreenSettings.newTips.Add("Scrambling Universal Constants...");
+            }
+        }
 
         internal static void LoadExternal(UrlDir.UrlConfig[] config)
         {
